4b69b2951a25720de7a10c63b804a87f9e393057
blocker147 <ilja.tarasovs@gmail.com>
2023-09-10 17:13:12 +0300

    Feature/9 exception handling (#23)
    * feat(9): exception handling
    
    * feat(9): exception handling

--- src/main/java/com/example/demo/service/DemoService.java
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -8,10 +8,18 @@ import java.util.Set;
 @Service
 public final class DemoService {
     private final Set<String> players;
+    private static final int SIZE_LIMIT = 10;
     public DemoService() {
         players = new HashSet<>();
     }
-    public boolean addPlayer(String player) { return players.add(player); }
+    public boolean addPlayer(String player) {
+        if (players.size() >= SIZE_LIMIT) throw new IllegalStateException("Limited amount of players are supported.");
+        if (players.add(player)) return true;
+        else throw new IllegalStateException("Player cannot be added because must be unique.");
+    }
     public Set<String> getPlayers() { return Set.copyOf(players); }
     public boolean playerExist(String player) { return players.contains(player); }
+    public void clearPlayers() {
+        players.clear();
+    }
 }
